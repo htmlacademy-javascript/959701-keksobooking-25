846e77f84ae985064613c6a7059dc1d591b2f7ff
Keks <keks@htmlacademy.ru>
2022-03-20 23:30:27 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,11 @@
+import { QUANTITY_OBJECTS } from './data.js';
+import { getNewRandomListings, getListing } from './listing-generator.js';
+import { createOfferTemplate } from './template-card.js';
+import './user-form.js';
+import './page-status.js';
+
+
+const [offer] = getNewRandomListings(QUANTITY_OBJECTS, getListing);
+const offerTemplate = createOfferTemplate(offer);
+
+document.querySelector('#map-canvas').append(offerTemplate);
---
+++ 7/js/user-form.js
@@ -0,0 +1,38 @@
+import { createPopup } from './util.js';
+
+// Валидация формы
+
+const errorTemplate = document.querySelector('#error').content;
+const successTemplate = document.querySelector('#success').content;
+const form = document.querySelector('.ad-form');
+
+const pristine = new Pristine(form, {
+  classTo: 'ad-form__element',
+  errorTextParent: 'ad-form__element'
+});
+
+// Проверка соответствия количества комнат количеству гостей.
+
+const roomNumberElement = form.querySelector('[name="rooms"]');
+const capacity = form.querySelector('[name="capacity"]');
+
+const maxGuests = {
+  1: ['1'],
+  2: ['1', '2'],
+  3: ['1', '2', '3'],
+  100: ['0']
+};
+
+const validateCapacity = () => maxGuests[roomNumberElement.value].includes(capacity.value);
+
+pristine.addValidator(capacity, validateCapacity, 'Количество гостей не соответствует количеству комнат');
+roomNumberElement.addEventListener('change', () => pristine.validate(capacity));
+
+form.addEventListener('submit', (evt) => {
+  if (pristine.validate()) {
+    return createPopup(successTemplate);
+  }
+  evt.preventDefault();
+  createPopup(errorTemplate);
+});
+
