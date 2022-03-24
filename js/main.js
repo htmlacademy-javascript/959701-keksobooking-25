cda7671ff55164dbeb5ebb327cb043bda4fcffe4
unknown <kristinamisenko9@gmail.com>
2022-03-24 23:24:46 +0300

    Модуль 9.1

--- js/main.js
+++ js/main.js
@@ -1,17 +1,9 @@
-import { QUANTITY_OBJECTS } from './data.js';
-import { getNewRandomListings, getListing } from './listing-generator.js';
-import { createOfferTemplate } from './template-card.js';
 import './user-form.js';
-import { deactivatePage, activatePage } from './page-status.js';
+import { offers, createMarker } from './map.js';
+import './slider.js';
 
+// Добавление объявлений
 
-const [offer] = getNewRandomListings(QUANTITY_OBJECTS, getListing);
-const offerTemplate = createOfferTemplate(offer);
-
-const mapCanvas = document.querySelector('#map-canvas');
-
-mapCanvas.append(offerTemplate);
-
-deactivatePage();
-
-mapCanvas.addEventListener('click', () => activatePage());
+offers.forEach((offer) => {
+  createMarker(offer);
+});
